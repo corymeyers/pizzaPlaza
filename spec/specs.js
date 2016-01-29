31cbfe9c4cccb5b97757c9a0df2152c99b91052e
cory meyers <whippoorwill.in.flight@gmail.com>
2016-01-29 09:12:17 -0800

    first bdd pass

--- spec/specs.js
+++ spec/specs.js
@@ -11,7 +11,5 @@ describe("PizzaToppings", function() {
   it ("collects user toppings input, tabulates the cost, and pushes it to a subtotal array", function() {
     var testPizzaOrder = new PizzaOrder("olive", "large", 14);
     expect(testPizzaOrder.PizzaToppings).to.equal("olive");
-    expect(testPizzaOrder.PizzaSize).to.equal("large");
-    expect(testPizzaOrder.PizzaPrice).to.equal(14);
-  });
+      });
 });
