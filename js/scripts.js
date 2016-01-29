86cfda66dfb22072ad172a892716ac0fc34f41ab
cory meyers <whippoorwill.in.flight@gmail.com>
2016-01-29 11:19:49 -0800

    second bdd pass

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,39 @@ function PizzaOrder (PizzaToppings, PizzaSize, PizzaPrice) {
   this.Tabulator = 0;
 }
 
-// PizzaOrder.prototype.PizzaToppings=function() {
-//   var subTotal = 0;
-//   if (document.getElementById("Pepperoni")=true {
-//     this.Tabulator + .5
-//
-//   })
+PizzaOrder.prototype.PizzaToppings=function() {
+  PizzaOrder.ToppingsSubtotal = 0;
+  if (this.Pepperoni = true) {
+    subTotal + .5;
+  }
+  if (this.Pickles == true) {
+    subTotal + .5;
+  }
+  if (this.Porridge == checked) {
+    subTotal + .5;
+  }
+  if (this.Peppercini == checked) {
+    subTotal + .5;
+  }
+  if (this.Potato == checked) {
+    subTotal + .5;
+  }
+  return subTotal;
+}
 
-
-
-
-//   PizzaOrder.Tabulator + subtotal
-// }
-
-
-
-
-
-
-
-
+PizzaOrder.prototype.PizzaSize=function() {
+  var PizzaSizeSubtotal = 0;
+  if (this.PizzaSize === "Small") {
+     PizzaSizeSubtotal += 10;
+    }
+  if (this.PizzaSize === "Medium") {
+     PizzaSizeSubtotal += 14;
+  }
+  if (this.PizzaSize === "Large") {
+     PizzaSizeSubtotal += 18;
+  }
+  return PizzaSizeSubtotal;
+}
 
 $(document).ready(function() {
   $("form#pizzaForm").submit(function(event) {
@@ -35,14 +48,8 @@ $(document).ready(function() {
     var Porridge = $("select#Porridge").val();
     var Peppercini = $("select#Peppercini").val();
     var Potato = $("select#Potato").val();
-    console.log(Potato)
 
-
-    // var movieTime = $("select#movieTime").val();
-    // var yearBorn = parseInt($("input#yearBorn").val());
-    // var newTicket = new Tickulator(movieType, movieTime, yearBorn);
-
-    $("ul#receipt").append("<li>"+"$"+"<span class='finalPrice'>" + PizzaOrder.price() + "</span></li>");
+    $("ul#receipt").append("<li>"+"$"+"<span class='finalPrice'>" + PizzaOrder.Tabulator() + "</span></li>");
       $("input").val("");
 
 });
